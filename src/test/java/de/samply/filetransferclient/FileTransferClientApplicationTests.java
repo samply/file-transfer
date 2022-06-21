b876e4401defd425c9de495a20d97c2e009e60fc
juarez <d.juarez@dkfz-heidelberg.de>
2022-06-21 15:32:13 +0200

    Add API for transferring files

---
+++ src/test/java/de/samply/filetransferclient/FileTransferClientApplicationTests.java
@@ -0,0 +1,13 @@
+package de.samply.filetransferclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileTransferClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
